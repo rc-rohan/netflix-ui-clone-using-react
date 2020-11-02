88af00471c0b5dc95c590e51c89b24fc673285a2
rc-rohan <rohan6hawat@gmail.com>
2020-11-03 02:15:30 +0530

    Added the rows images from tmdb api and the basic styling to the images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Row from "./Components/Row";
+import requests from "./request";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello world
+      <Row
+        title="NFLIX ORIGINALS"
+        isLargeRow
+        fetchURL={requests.fetchNetflixOriginals}
+      />
+      <Row title="Top Rated" fetchURL={requests.fetchTrending} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
---
+++ src/Components/Row.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import axios from "../axios";
+import './Row.css'
+
+function Row({ title, fetchURL,isLargeRow }) {
+  const base_url = "https://image.tmdb.org/t/p/original/";
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
+    }
+    fetchData();
+    // in useEffect we need to add any of the variable as the dependency if that is used inside useEffect and that keeps on changing so here in this case we add up the  fetchURL
+  }, [fetchURL]);
+  console.log(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+            key={movie.id}
+            src={`${base_url}${isLargeRow?movie.poster_path:movie.backdrop_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
